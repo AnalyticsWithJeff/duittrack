293a55736a995f91e4b1bc2a687abea81053182d
cavenhsangve <jadencaven@gmail.com>
2024-12-11 19:15:18 +0800

    screen sizes

--- duit-track/tailwind.config.js
+++ duit-track/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+      screens: {
+        'phone': '350px',
+        // => @media (min-width: 350px) { ... }
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
       extend: {},
     },
     variants: {
